0aacff08902cb726f6f8005cb0af0e78fb443da4
Paulo Castagnari <paulo.vcastagnari@gmail.com>
2022-05-14 01:22:32 -0300

    path

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,10 @@ export default function Custom404() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/");
+    let path = window.location.pathname;
+    console.log(window.location.pathname);
+
+    router.push(`/?src=${path?.replace("/", "")}`);
   }, []);
 
   return null;
