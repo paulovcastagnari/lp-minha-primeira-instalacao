42a47ebc1c5dcbceb29320319433e00f75f2a563
Paulo Castagnari <paulo.vcastagnari@gmail.com>
2022-05-20 11:38:20 -0300

    attempt-to-fix-errors

--- util/string/transformInputText.ts
+++ util/string/transformInputText.ts
@@ -19,37 +19,43 @@ export const transformInputText = (
       transformedText = text.toLowerCase();
     }
 
-    if (filter === "CHAR_ONLY") {
+    if (filter === "CHAR_ONLY" && transformedText.replaceAll) {
       transformedText = transformedText.replaceAll(
         /[^A-Za-z" "çàãáâéèêíìîóôòõúùûÇÁÃÀÂÉÈÊÍÌÎÓÕÔÒÚÙÛ]/g,
         ""
       );
-    } else if (filter === "CHAR_AND_NUM") {
+    } else if (filter === "CHAR_AND_NUM" && transformedText.replaceAll) {
       transformedText = transformedText.replaceAll(
         /[^A-Za-z0-9" "çàãáâéèêíìîóôòõúùûÇÁÃÀÂÉÈÊÍÌÎÓÕÔÒÚÙÛ]/g,
         ""
       );
-    } else if (filter === "CHAR_NUM_AND_UNDER") {
+    } else if (filter === "CHAR_NUM_AND_UNDER" && transformedText.replaceAll) {
       transformedText = transformedText.replaceAll(
         /[^A-Za-z0-9" "çàãáâéèêíìîóôòõúùûÇÁÃÀÂÉÈÊÍÌÎÓÕÔÒÚÙÛ_]/g,
         ""
       );
-    } else if (filter === "CHAR_ONLY_NO_BLANK") {
+    } else if (filter === "CHAR_ONLY_NO_BLANK" && transformedText.replaceAll) {
       transformedText = transformedText.replaceAll(
         /[^A-Za-zçàãáâéèêíìîóôòõúùûÇÁÃÀÂÉÈÊÍÌÎÓÕÔÒÚÙÛ]/g,
         ""
       );
-    } else if (filter === "CHAR_AND_NUM_NO_BLANK") {
+    } else if (
+      filter === "CHAR_AND_NUM_NO_BLANK" &&
+      transformedText.replaceAll
+    ) {
       transformedText = transformedText.replaceAll(
         /[^A-Za-z0-9çàãáâéèêíìîóôòõúùûÇÁÃÀÂÉÈÊÍÌÎÓÕÔÒÚÙÛ]/g,
         ""
       );
-    } else if (filter === "CHAR_AND_UNDER_NO_BLANK") {
+    } else if (
+      filter === "CHAR_AND_UNDER_NO_BLANK" &&
+      transformedText.replaceAll
+    ) {
       transformedText = transformedText.replaceAll(
         /[^A-Za-z_çàãáâéèêíìîóôòõúùûÇÁÃÀÂÉÈÊÍÌÎÓÕÔÒÚÙÛ]/g,
         ""
       );
-    } else if (filter === "CONTENT") {
+    } else if (filter === "CONTENT" && transformedText.replaceAll) {
       transformedText = transformedText.replaceAll(
         /[^A-Za-z0-9-()$@%*=+;:,\._?/!" "çàãáâéèêíìîóôòõúùûÇÁÃÀÂÉÈÊÍÌÎÓÕÔÒÚÙÛ]/g,
         ""
